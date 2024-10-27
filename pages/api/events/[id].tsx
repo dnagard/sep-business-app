f455ae425d68c0b87ef3fc6d16f4712e08890ec1
dnagard <nagarddaniel@gmail.com>
2024-10-27 22:35:19 +0100

    An api for requesting event details as well as changing the status and nextAction user

--- pages/api/events/[id].tsx
+++ pages/api/events/[id].tsx
@@ -8,11 +8,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   if (req.method === 'GET') {
     try {
-      const event = await prisma.eventRequest.findUnique({ where: { id: Number(id) } });
+      const event = await prisma.eventRequest.findUnique({
+        where: { id: Number(id) },
+      });
+      if (!event) {
+        return res.status(404).json({ error: "Event not found" });
+      }
       res.status(200).json(event);
     } catch (error) {
-      console.log('GET error:', error);
-      res.status(500).json({ error: 'Error fetching event' });
+      console.error(error);
+      res.status(500).json({ error: "Error fetching event" });
     }
   } else if (req.method === 'PATCH') {
     const { status, nextAction } = req.body;

cc4bd3883fa5ae32778bbdd2d4d9d02578ee48ef
dnagard <nagarddaniel@gmail.com>
2024-10-27 22:34:45 +0100

    added the appropriate links to route to the event list

--- app/(dashboards)/seniorcustomerservice/page.tsx
+++ app/(dashboards)/seniorcustomerservice/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function SeniorCustomerService() {
     return (
       <div className="flex flex-col items-center min-h-screen p-6 bg-gray-100">
@@ -5,16 +7,18 @@ export default function SeniorCustomerService() {
           Senior Customer Service Officer Dashboard
         </h1>
 
-        <div className="flex gap-8">
-          <div className="w-64 h-80 bg-white shadow-lg rounded-lg transform transition-transform hover:scale-105 p-6 flex flex-col justify-center items-center">
-            <h2 className="text-2xl font-semibold mb-4 text-center">
-              Review Event Requests
-            </h2>
-            <p className="text-gray-600 text-center">
-              Review open events. Actionable items are highlighted.
-            </p>
+        <Link href="/viewRequests">
+          <div className="flex gap-8">
+            <div className="w-64 h-80 bg-white shadow-lg rounded-lg transform transition-transform hover:scale-105 p-6 flex flex-col justify-center items-center">
+              <h2 className="text-2xl font-semibold mb-4 text-center">
+                Review Event Requests
+              </h2>
+              <p className="text-gray-600 text-center">
+                Review open events. Actionable items are highlighted.
+              </p>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     );
 }

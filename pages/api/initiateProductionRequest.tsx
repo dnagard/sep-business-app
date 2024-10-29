334a59364974924a5e4c0df53e19a7690b075704
dnagard <nagarddaniel@gmail.com>
2024-10-29 09:57:22 +0100

    Refactor taskRequestPM component and add API endpoint for initiating production requests

---
+++ pages/api/initiateProductionRequest.tsx
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { team, activity, budget, personell, dueDate } = req.body;
+
+    try {
+      const serviceRequest = await prisma.productionRequest.create({
+        data: {
+          team,
+          activity,
+          budget,
+          personell,
+          dueDate,
+        },
+      });
+
+      res.status(201).json(serviceRequest);
+    } catch (error) {
+      console.error("Error creating serviceRequest:", error);
+      res.status(500).json({ error: "Failed to create task request" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
